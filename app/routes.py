b0fe71beadab0c8e0ce0564408a058c80a1a65da
elmaroufa <salyabbo@gmail.com>
2023-11-23 12:26:32 +0100

    feature post in index page

--- app/routes.py
+++ app/routes.py
@@ -3,15 +3,26 @@ from flask import render_template,flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username' : 'Saly'}
-    return render_template('index.html', title='home', user=user)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user )
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live !')
+        return redirect(url_for('index'))
+    posts = [
+        {'author': {'username': 'John'}, 'body': 'Beautiful day in Portland'},
+        {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so'}
+    ]
+    
+    return render_template('index.html', title='home', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -48,7 +59,11 @@ def register():
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    posts = [
+        {'author': {'username': 'John'}, 'body': 'Beautiful day in Portland'},
+        {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so'}
+    ]
+    return render_template('register.html', title='Register', form=form, posts=posts)
 
 @app.route('/user/<username>')
 @login_required
