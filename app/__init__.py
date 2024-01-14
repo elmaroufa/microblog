e000cf401158514a139831406d36ab4c707204a0
elmaroufa <salyabbo@gmail.com>
2024-01-14 19:18:41 +0100

    feature boostrap in view

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
