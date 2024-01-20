6131d75fd5fd299ca7a507b357a9778760655eb0
elmaroufa <salyabbo@gmail.com>
2024-01-20 07:10:38 +0100

    feature add commande cli

--- microblog.py
+++ microblog.py
@@ -1,6 +1,9 @@
-from app import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db, cli
 from app.models import User, Post
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
